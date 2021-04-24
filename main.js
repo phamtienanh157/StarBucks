2682f9f3314eca7ab2aa2f8ca6445b6efbc17243
Phạm Tiến Ánh <phamtienanh157@gmail.com>
2021-04-24 17:35:17 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlide(item) {
+  const image = document.querySelector(".imgStarBucks");
+  image.src = item;
+}
+function changeColor(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
